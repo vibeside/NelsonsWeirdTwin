d208bdb416e4a2f14fa841227b62b54dd3c12db8
vibeside <vibeside01@gmail.com>
2025-04-10 07:25:19 -0400

    discord.net switch

--- NelsonsWeirdTwin/Program.cs
+++ NelsonsWeirdTwin/Program.cs
@@ -1,88 +1,103 @@
-﻿using DotNetEnv;
-using NetCord;
-using NetCord.Gateway;
-using NetCord.Rest;
-using NetCord.Services;
-using NetCord.Services.ApplicationCommands;
+﻿using Discord;
+using Discord.Commands;
+using Discord.Net;
+using Discord.WebSocket;
+using DotNetEnv;
+using Newtonsoft.Json;
+using System;
 using System.Text.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace NelsonsWeirdTwin
 {
     internal class Program
     {
-        public static IEnumerable<KeyValuePair<string, string>> env = Env.Load();
-        
-        static string botToken = Environment.GetEnvironmentVariable("TOKEN");
+        static string botToken;
 
-        public static GatewayClient client = new(new BotToken(botToken),new GatewayClientConfiguration()
-        {
-            Intents = GatewayIntents.All
-        });
+        static DiscordSocketClient _client;
+        static CommandService _commandService;
 
         static Dictionary<string,string> triggerWordsAndResponses;
 
-        public static ApplicationCommandService<SlashCommandContext> appCommandServices = new();
 
         static async Task Main()
         {
-            triggerWordsAndResponses = JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText("triggers.json"));
-            client.Log += message =>
-            {
-                Console.WriteLine(message);
-                return default;
-            };
+            DotNetEnv.Env.Load();
+            botToken = Environment.GetEnvironmentVariable("TOKEN");
+            triggerWordsAndResponses = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("triggers.json"));
+            
+            _client = new DiscordSocketClient();
 
-            appCommandServices.AddSlashCommand("pingmodtesters", "Pings the mod tester role", PingModTesters);
-            appCommandServices.AddSlashCommand("verifyuser", "Swaps a users unverified and verified role.", VerifyCommand);
-            appCommandServices.AddSlashCommand("addtrigger", "Adds a trigger word and phrase", AddTriggerAndResponse);
-            client.InteractionCreate += async interaction =>
-            {
-                if (interaction is not SlashCommandInteraction appCommandInteraction) return;
-                var result = await appCommandServices.ExecuteAsync(new SlashCommandContext(appCommandInteraction, client));
-                
-                if (result is not IFailResult fail) return;
+            _client.Log += Log;
 
-                try
-                {
-                    await interaction.SendResponseAsync(InteractionCallback.Message(fail.Message));
-                }
-                catch
-                {
+            _client.Ready += Client_Ready;
 
-                }
-            };
-            client.MessageCreate += MessageReceived;
-            await appCommandServices.CreateCommandsAsync(client.Rest, client.Id);
-            await client.StartAsync();
+            await _client.LoginAsync(TokenType.Bot,botToken);
+            await _client.StartAsync();
             while (true)
             {
                 if (Console.ReadLine() == "ex")break; 
-                if(Console.ReadLine() == "u") triggerWordsAndResponses = JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText("triggers.json"));
+                if(Console.ReadLine() == "u") triggerWordsAndResponses = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("triggers.json"));
             }
         }
-
-        static async ValueTask MessageReceived(Message msg)
+        public static async Task Client_Ready()
         {
+            List<SlashCommandBuilder> builders = new List<SlashCommandBuilder>();
+            builders.Add(new());
+            builders[0].WithName("pingmodtesters");
+            builders[0].WithDescription("Pings the Mod tester role!");
+            builders.Add(new());
+            builders[1].WithName("addtrigger");
+            builders[1].WithDescription("Adds a trigger and response to the bot");
+            builders.Add(new()); // to be implemented, not sure how to avoid bricking it aside
+            builders[2].WithName("edittrigger"); // from creating a copy entry and placing it
+            builders[2].WithDescription("Edits an existing trigger"); 
+            builders.Add(new()); 
+            builders[3].WithName("verifyuser");
+            builders[3].WithDescription("Swaps a user to the verified mod developer role!");
 
-            if (msg.Author.Id == client.Id) return;
-            if (msg.Author.IsBot) return;
-            if (msg.Author.IsSystemUser == true) return;
-            foreach (string k in triggerWordsAndResponses.Keys)
+            try
             {
-                if (msg.Content.Contains(k))
+                foreach (SlashCommandBuilder c in builders)
                 {
-                    await client.Rest.SendMessageAsync(msg.ChannelId, triggerWordsAndResponses[k]);
+                    await _client.CreateGlobalApplicationCommandAsync(c.Build());
                 }
             }
-            return;
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
 
+                Console.WriteLine(json);
+            }
         }
+        public static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg);
+            return Task.CompletedTask;
+        }
+
+        //static async ValueTask MessageReceived(Message msg)
+        //{
+
+        //    if (msg.Author.Id == client.Id) return;
+        //    if (msg.Author.IsBot) return;
+        //    if (msg.Author.IsSystemUser == true) return;
+        //    foreach (string k in triggerWordsAndResponses.Keys)
+        //    {
+        //        if (msg.Content.Contains(k))
+        //        {
+        //            await client.Rest.SendMessageAsync(msg.ChannelId, triggerWordsAndResponses[k]);
+        //        }
+        //    }
+        //    return;
+
+        //}
         static Task<string> AddTriggerAndResponse(string trigger, string response) 
         {
             string replaced = response.Replace("\\\\","\\");
             response = replaced.Replace("\\n","\n");
             triggerWordsAndResponses.Add(trigger, response);
-            File.WriteAllText("triggers.json", JsonSerializer.Serialize(triggerWordsAndResponses));
+            File.WriteAllText("triggers.json", JsonConvert.SerializeObject(triggerWordsAndResponses));
             return Task<string>.FromResult($"Trigger {trigger}:{response} added.");
         }
         static async Task<string> PingModTesters()
@@ -90,12 +105,12 @@ namespace NelsonsWeirdTwin
             await Task.Delay(10);
             return "<@&1356043224510103692>";
         }
-        static async Task<string> VerifyCommand(GuildUser user)
-        {
-            ulong userID = user.Id;
-            await user.AddRoleAsync(1357079958954049717);
-            await user.RemoveRoleAsync(1354831227152109590);
-            return $"<@{userID}> has been made a Verified Mod Developer!";
-        }
+        //static async Task<string> VerifyCommand(GuildUser user)
+        //{
+        //    ulong userID = user.Id;
+        //    await user.AddRoleAsync(1357079958954049717);
+        //    await user.RemoveRoleAsync(1354831227152109590);
+        //    return $"<@{userID}> has been made a Verified Mod Developer!";
+        //}
     }
 }
