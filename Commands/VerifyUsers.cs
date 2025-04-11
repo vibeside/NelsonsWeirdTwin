68475fd36c022aec1143982bf296ced8dcdbf604
vibeside <vibeside01@gmail.com>
2025-04-10 21:48:35 -0400

    added verifyusers for real this time

---
+++ Commands/VerifyUsers.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NelsonsWeirdTwin.Commands
+{
+    internal class VerifyUsers : Command
+    {
+        internal override SlashCommandProperties CommandProperties =>
+        new SlashCommandBuilder()
+            .WithName("verifyuser")
+            .WithDescription("Verifies user thats input")
+            .AddOption(
+                "user",
+                ApplicationCommandOptionType.User,
+                "User to be verified",
+                isRequired:true)
+            .Build();
+        internal override Task OnExecuted(DiscordSocketClient client, SocketSlashCommand context)
+        {
+            Console.WriteLine("Executing.");
+            if (context.Data.Options.First().Value is SocketGuildUser user)
+            {
+                Console.WriteLine(user.Username); // verify
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
