4f891c80fe1ba6e3074fc2c2302f62862c02fe3b
vibeside <vibeside01@gmail.com>
2025-04-12 12:00:26 -0400

    fixed branch issues

--- Commands/Purge.cs
+++ Commands/Purge.cs
@@ -6,69 +6,71 @@ using Discord.WebSocket;
 
 namespace NelsonsWeirdTwin.Commands;
 
-internal class PurgeCommand: Command
+internal class PurgeCommand : Command
 {
-	private const int MaxPurgeAmount = 200;
-	
-	internal override SlashCommandProperties CommandProperties =>
-		new SlashCommandBuilder()
-			.WithName("purge")
-			.WithDescription("Purge messages from a channel.")
-			.AddOption(
-				new SlashCommandOptionBuilder()
-					.WithName("upto")
-					.WithDescription("Delete up to a certain message ID.")
-					.WithType(ApplicationCommandOptionType.String)
-					.WithRequired(false)
-			)
-			.AddOption(
-				new SlashCommandOptionBuilder()
-					.WithName("from")
-					.WithDescription("Delete only a specific user's messages.")
-					.WithType(ApplicationCommandOptionType.User)
-					.WithRequired(false)
-			)
-			.Build();
+    private const int MaxPurgeAmount = 200;
 
-	internal override async Task OnExecuted(DiscordSocketClient client, SocketSlashCommand context)
-	{
-		string upto = context.Data.Options.FirstOrDefault(opt => opt.Name == "upto")?.Value as string;
-		IUser user = context.Data.Options.FirstOrDefault(opt => opt.Name == "from")?.Value as IUser;
-		if (context.Channel is not ITextChannel channel)
-		{
-			await context.RespondAsync("This command can only be used in text channels.");
-			return;
-		}
+    internal override SlashCommandProperties CommandProperties =>
+    new SlashCommandBuilder()
+    .WithName("purge")
+    .WithDescription("Purge messages from a channel.")
+    .AddOption(
+    new SlashCommandOptionBuilder()
+    .WithName("upto")
+    .WithDescription("Delete up to a certain message ID.")
+    .WithType(ApplicationCommandOptionType.String)
+    .WithRequired(true)
+    )
+    .AddOption(
+    new SlashCommandOptionBuilder()
+    .WithName("from")
+    .WithDescription("Delete only a specific user's messages.")
+    .WithType(ApplicationCommandOptionType.User)
+    .WithRequired(false)
+    )
+    .Build();
 
-		if (upto is null)
-		{
-			await context.RespondAsync("You must specify either an amount, or up to a message ID, to purge.");
-			return;
-		}
-		
-		var purged = 0;
-		
-		if (upto is not null)
-		{
-			var messages = await channel.GetMessagesAsync(limit: MaxPurgeAmount).FlattenAsync();
-			if (user != null) messages = messages.Where(m => m.Author.Id == user.Id);
-			messages = messages.Where(m => m.CreatedAt > DateTimeOffset.UtcNow.AddDays(-14)).ToList(); // Discord only allows purging messages from the last 14 days.
-			
-			var uptoMessage = messages.FirstOrDefault(m => m.Id.ToString() == upto);
-			if (uptoMessage == null)
-			{
-				await context.RespondAsync($"Could not find message with ID {upto}. It may be past the {MaxPurgeAmount} message purge limit, or it may not exist.");
-				return;
-			}
-			messages = messages.Where(m => m.CreatedAt > uptoMessage.CreatedAt).ToList();
-			
-			purged = messages.Count();
-			await channel.DeleteMessagesAsync(messages);
-		}
-		
-		await context.RespondAsync($"Purged {purged} {Utils.Plural(purged, "message", "messages")}.");
-		
-		await Task.Delay(2000);
-		await context.DeleteOriginalResponseAsync();
-	}
+    internal override async Task OnExecuted(DiscordSocketClient client, SocketSlashCommand context)
+    {
+        var upto = context.Data.Options.FirstOrDefault(opt => opt.Name == "upto")?.Value as string;
+        var user = context.Data.Options.FirstOrDefault(opt => opt.Name == "from")?.Value as IUser;
+        if (context.Channel is not ITextChannel channel)
+        {
+            await context.RespondAsync("This command can only be used in text channels.");
+            return;
+        }
+
+        if (upto is null)
+        {
+            await context.RespondAsync("You must specify a message ID to purge up to.");
+            return;
+        }
+
+        var purged = 0;
+        var messages = (await channel.GetMessagesAsync(limit: MaxPurgeAmount).FlattenAsync()).ToList();
+
+        var uptoMessage = messages.FirstOrDefault(m => m.Id.ToString() == upto);
+        if (uptoMessage == null)
+        {
+            await context.RespondAsync($"Could not find message with ID {upto}. It may be past the {MaxPurgeAmount} message purge limit, or it may not exist.");
+            return;
+        }
+
+        if (user != null) messages.RemoveAll(m => m.Author.Id != user.Id);
+        messages.RemoveAll(m => m.CreatedAt > DateTimeOffset.UtcNow.AddDays(-14)); // Discord only allows purging messages from the last 14 days.
+        messages.RemoveAll(m => m.CreatedAt > uptoMessage.CreatedAt); // Remove all messages after the specified message.
+
+        purged = messages.Count;
+        if (purged == 0)
+        {
+            await context.RespondAsync("No messages to purge.");
+            return;
+        }
+
+        await channel.DeleteMessagesAsync(messages);
+        await context.RespondAsync($"Purged {purged} {Utils.Plural(purged, "message", "messages")}.");
+
+        await Task.Delay(2000);
+        await context.DeleteOriginalResponseAsync();
+    }
 }
