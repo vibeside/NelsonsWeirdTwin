90f4c5406a38d86e0248946f221209fdb8b09e2a
vibeside <vibeside01@gmail.com>
2025-04-15 01:15:00 -0400

    included the files, see last commit

---
+++ Commands/ChatModApplication.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NelsonsWeirdTwin.Commands
+{
+    internal class ChatModApplication : Command
+    {
+        internal override SlashCommandProperties CommandProperties =>
+        new SlashCommandBuilder()
+            .WithName("chatmod")
+            .WithDescription("Makes current channel into a role picker channel")
+        .Build();
+
+        internal override async Task OnExecuted(DiscordSocketClient client, SocketSlashCommand context)
+        {
+            ComponentBuilder cb = new ComponentBuilder();
+            cb.WithButton(
+                new ButtonBuilder()
+                .WithCustomId("vouch")
+                .WithLabel("I vouch!")
+                .WithEmote(Emote.Parse("ducky"))
+            );
+            cb.WithButton(
+                new ButtonBuilder()
+                .WithCustomId("anti-vouch")
+                .WithLabel("I don't vouch!")
+                .WithEmote(Emote.Parse("AntiDucky"))
+            );
+            await context.RespondAsync(components: cb.Build());
+        }
+
+
+        public static async Task HandleVouch(SocketMessageComponent c)
+        {
+
+        }
+        public static async Task HandleAntiVouch(SocketMessageComponent c)
+        {
+
+        }
+    }
+}
